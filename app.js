c2fbd96f0c1881ab26d9e8da275bfc03be5c475b
Van <Van@MyPlan.works>
2018-07-02 08:40:53 -0500

    cleaned up code

--- app.js
+++ app.js
@@ -10,42 +10,30 @@ var User = require('./models/user');
 var Expense = require('./models/budgets_expenses');
 var Budget = require('./models/budgets_bycat');
 var path = require('path');
-
 let pgp = require('pg-promise')()
 let connectionString = 'postgres://instabudget:digitalcrafts@instabudget.cuzupkl5r98f.us-east-2.rds.amazonaws.com:5432/InstaBudget'
+//let connectionString = 'postgres://localhost:5432/instabudget'
 let db = pgp(connectionString)
 var status = ''
-
-
-//let connectionString = 'postgres://localhost:5432/instabudget'
-
 // invoke an instance of express application.
 var app = express();
-
 // set static path
 app.use(express.static(path.join(__dirname, 'public')))
-
 // set mustache views engine
 app.engine('mustache', mustacheExpress());
-
 // mustache pages will be inside the views folder
 app.set('views', './views');
 app.set('view engine', 'mustache');
-
 // set our application port
 app.set('port', 3000);
-
 // set morgan to log info about our requests for development use.
 app.use(morgan('dev'));
-
 // initialize body-parser to parse incoming parameters requests to req.body
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
 // initialize cookie-parser to allow us access the cookies stored in the browser. 
 app.use(cookieParser());
-
 // initialize express-session to allow us track the logged-in user across sessions.
 app.use(session({
     key: 'user_sid',
@@ -56,7 +44,6 @@ app.use(session({
         expires: 600000
     }
 }));
-
 // This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
 // This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
 app.use((req, res, next) => {
@@ -65,7 +52,6 @@ app.use((req, res, next) => {
     }
     next();
 });
-
 // middleware function to check for logged-in users
 var sessionChecker = (req, res, next) => {
     if (req.session.user && req.cookies.user_sid) {
@@ -74,13 +60,10 @@ var sessionChecker = (req, res, next) => {
         next();
     }
 };
-
 // route for Home-Page
 app.get('/', sessionChecker, (req, res) => {
     res.redirect('/login');
 });
-
-
 // route for user signup
 app.route('/signup')
     .get(sessionChecker, (req, res) => {
@@ -100,7 +83,6 @@ app.route('/signup')
                 res.redirect('/signup');
             });
     });
-
 // route for user Login
 app.route('/login')
     .get(sessionChecker, (req, res) => {
@@ -109,7 +91,6 @@ app.route('/login')
     .post((req, res) => {
         var username = req.body.username,
             password = req.body.password;
-
         User.findOne({
             where: {
                 username: username
@@ -125,15 +106,11 @@ app.route('/login')
             }
         });
     });
-
-
 // route for user's dashboard
 app.get('/dashboard', (req, res) => {
     let usernum = req.session.user.id
     if (req.session.user && req.cookies.user_sid) {
-
         fetchUserBudgetCategories(usernum, (budgetCategories) =>{
-                
             res.render('dashboard', {
                 budgetCategories: budgetCategories
             })
@@ -142,7 +119,6 @@ app.get('/dashboard', (req, res) => {
         res.redirect('/login');
     }
 });
-
 // route to add quick expense
 app.post('/add_quick_expense', (req, res) => {
     if (req.session.user && req.cookies.user_sid) {
@@ -155,35 +131,26 @@ app.post('/add_quick_expense', (req, res) => {
                 res.redirect('/quickexpense');
             })
             .catch(error => {
-                
                 res.redirect('/quickexpense');
-                
             });
         // res.redirect('/quickexpense');
     } else {
         res.redirect('/login');
-        
     }
 });
-
 // route to add quick expense
 app.post('/')
-
 // fectchQuickExpenses Function with callback as argument
 function fetchQuickExpenses(usernum, callback) {
-
     Expense.findAll({ where: {
         userid: usernum,
         islogged: 'FALSE'
     }}).then(function (expenses) {
-
             callback(expenses);
         });
 };
-
 // fetchUserBudgetCategories
 function fetchUserBudgetCategories(usernum, callback) {
-
     Budget.findAll({
         attributes: ['name', 'id'],
         where: {
@@ -194,14 +161,10 @@ function fetchUserBudgetCategories(usernum, callback) {
 }
 // route for Quick expense page
 app.get('/quickexpense', (req, res) => {
-
     let usernum = req.session.user.id
     if (req.session.user && req.cookies.user_sid) {
-
         fetchQuickExpenses(usernum, (expenses) => {
-
             fetchUserBudgetCategories(usernum, (budgetCategories) =>{
-                
                 res.render('quickexpense', {
                     itemList: expenses,
                     budgetCategories: budgetCategories
@@ -212,13 +175,10 @@ app.get('/quickexpense', (req, res) => {
         res.redirect('/login');
     }
 });
-
 // route to handle deleteQuickExpense request
 app.post('/deleteQuickExpense/:id', (req, res) => {
     let expenseid = req.params.id
-
     if (req.session.user && req.cookies.user_sid) {
-
         Expense.destroy({
             where: {
                 id: expenseid
@@ -226,19 +186,15 @@ app.post('/deleteQuickExpense/:id', (req, res) => {
         }).then(function () {
             res.redirect('/quickexpense');
         })
-
     } else {
         res.redirect('/login');
     }
 });
-
 // route to Log Quick Expense
 app.post('/log_quick_expense', (req, res) =>{
     let expenseid = req.body.expenseid
     let budgetCategory = req.body.budgetCategory
-   
     if (req.session.user && req.cookies.user_sid) {
-        
         Expense.update({
             islogged: 'TRUE',
             category: budgetCategory},{
@@ -248,19 +204,14 @@ app.post('/log_quick_expense', (req, res) =>{
         }).then(function () {
             res.redirect('/quickexpense');
         })
-
     } else {
         res.redirect('/login');
     }
 });
-
 // route to Log Expense from Dashboard
 app.post('/log_expense', (req, res) =>{
-
-   
     if (req.session.user && req.cookies.user_sid) {
         if(req.body.budgetName == ''){
-        
         Expense.create({
             islogged: 'TRUE',
             category: req.body.budgetCategory,
@@ -272,7 +223,6 @@ app.post('/log_expense', (req, res) =>{
         })
         .catch(error => {
             res.redirect('/dashboard');
-            
         });
     }
     else {
@@ -280,7 +230,6 @@ app.post('/log_expense', (req, res) =>{
             name: req.body.budgetName,
             budget: req.body.budgetAmount,
             userid: req.session.user.id,
-            
         }).then(name => {
             Expense.create({
                 islogged: 'TRUE',
@@ -290,25 +239,19 @@ app.post('/log_expense', (req, res) =>{
                 userid: req.session.user.id
             })
         })
-            
-            
         .then(function (){
             res.redirect('/dashboard');
         }).catch(error => {
             res.redirect('/dashboard');
         })
-
     }
 } else {
     res.redirect('/login');
 }      
 });
-
 // route to set budget
 app.post('/set_budget', (req, res) => {
-
     if(req.session.user && req.cookies.user_sid){
-
         Budget.create({
             name: req.body.budgetName,
             budget: req.body.budgetAmount,
@@ -322,7 +265,6 @@ app.post('/set_budget', (req, res) => {
         res.redirect('/login');
     }
 })
-
 // route for Budget tracking page
 app.get('/tracking', (req, res) => {
     if (req.session.user && req.cookies.user_sid) {
@@ -346,8 +288,6 @@ app.get('/tracking', (req, res) => {
                 thebudget['catper'] = 0
                 thebudget['status'] = 'GreenYellow '
                 thebudget['header'] = ' '
-
-            
                 for(var j = 0 ; j < allItems.length;j++){
                     var theItem = allItems[j]
                     if (theItem.category != null){
@@ -370,17 +310,12 @@ app.get('/tracking', (req, res) => {
                     }
                     else{
                         if (thebudget.id == 'Unassigned' ){
- // Trying to pass a mustache tag
                             thebudget['header'] = 'none'
                             thebudget['expenses'].push(theItem)
                             thebudget['tally']=parseFloat(thebudget['tally'])+parseFloat(theItem.amount)
                         }
-                        }
-                       
+                        }                     
                 } 
-               
-               
-
                 budgetsWithExpenses.push(thebudget)
             }
             Expense.sum('amount',{where:{userid : usernum} }).then((sumA) =>{
@@ -402,12 +337,9 @@ app.get('/tracking', (req, res) => {
         })
     } else {res.redirect('/login');}
 });
-
 app.post('/deleteExpense/:id', (req, res) => {
     let expenseid = req.params.id
-
     if (req.session.user && req.cookies.user_sid) {
-
         Expense.destroy({
             where: {
                 id: expenseid
@@ -415,14 +347,10 @@ app.post('/deleteExpense/:id', (req, res) => {
         }).then(function () {
             res.redirect('/tracking');
         })
-
     } else {
         res.redirect('/login');
     }
 });
-
-
-
 // route for user logout
 app.get('/logout', (req, res) => {
     if (req.session.user && req.cookies.user_sid) {
@@ -432,17 +360,9 @@ app.get('/logout', (req, res) => {
         res.redirect('/login');
     }
 });
-
-
 // route for handling 404 requests(unavailable routes)
 app.use(function (req, res, next) {
     res.status(404).send("Sorry can't find that!")
 });
-
-
 // start the express server
 app.listen(app.get('port'), () => console.log(`App started on port ${app.get('port')}`));
-
-
-
-
